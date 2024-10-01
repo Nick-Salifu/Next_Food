3de03f0f1832eb9bfd87f823d5a4386456d006ce
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2024-10-02 00:28:06 +0100

    adding sqlite database

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,9 +1,23 @@
-import Link from "next/link";
+import Link from "next/link"
+import styles from "./page.module.css"
+import MealsGrid from "@/components/meals/meals-grid"
 
 export default function Meals() {
     return (
-        <div>
-            <h1>Welcome to your meals section</h1>
-        </div>     
+       <>
+            <header className={styles.header}>
+                <h1>
+                    Delicious meals, created{' '}
+                    <span className={styles.highlight}>by you</span>
+                </h1>
+                <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
+                <p className={styles.cta}>
+                    <Link href="/meals/share">Share Your Favorite Recipe</Link>
+                </p>
+            </header>
+            <main className={styles.main}>
+                <MealsGrid meals={[]} />
+            </main>
+       </>     
     )
 }
